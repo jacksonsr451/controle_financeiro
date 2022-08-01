f93541c5628e08c990eabaaba14fdcc5a35a64f9
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-01 15:30:47 -0300

    include resource /receitas for methods get and post

--- app/blueprints/api/__init__.py
+++ app/blueprints/api/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint 
 from flask_restful import Api
 
+from app.blueprints.api.receita import Receita
+
 
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(blueprint)
-# api.add_resource()
+api.add_resource(Receita, "/receitas")
 
 def init_app(app):
     app.register_blueprint(blueprint)
