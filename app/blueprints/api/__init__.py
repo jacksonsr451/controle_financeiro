e1adc6dfc45a0bbe53c4de6ff302a69acd2c989b
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-02 18:53:22 -0300

    Include route /despesas by method get and post

--- app/blueprints/api/__init__.py
+++ app/blueprints/api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint 
 from flask_restful import Api
+from app.blueprints.api.despesas import Despesas
 
 from app.blueprints.api.receita import Receita, ReceitaByID
 
@@ -8,6 +9,7 @@ blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(blueprint)
 api.add_resource(Receita, "/receitas")
 api.add_resource(ReceitaByID, "/receitas/<id>")
+api.add_resource(Despesas, "/despesas")
 
 
 def init_app(app):
---
+++ app/blueprints/api/despesas.py
@@ -0,0 +1,12 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse
+
+
+
+class Despesas(Resource):
+    def get(self):
+        pass
+    
+    
+    def post(self, id):
+        pass
