922cc41ca506db95a3bec9cce2527d3f4f24af98
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-03 15:30:59 -0300

    Test status code 200 in get despesas

--- app/tests/despesas/test_get_all_despesas.py
+++ app/tests/despesas/test_get_all_despesas.py
@@ -1,16 +1,32 @@
+import os
 from unittest import TestCase
-import unittest
-
-from flask import Flask
+from flask import current_app
 
 from app import app
+from app.ext.flask_sqlalchemy import db
 
 
 class TestGetAllDespesas(TestCase):
-    def setUp(self) -> None:
-        self.app = app.create_app()
+    URL = "http://localhost:5000/api/v1/despesas"
         
     
-    def test_if_app_instance_of(self):
-        self.assertIsInstance(self.app, Flask)
+    def setUp(self) -> None:
+        app_test = app.create_app()
+        app_test.testing = True
+        self.ctx = app_test.app_context()
+        self.ctx.push()
+        self.app = app_test.test_client()
+        db.create_all()
+        
+        
+    def test_shold_be_request_return_status_code_200(self):
+        resp = self.app.get(self.URL)
+        self.assertEqual(resp.status_code, 200)
+        
+        
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+        self.ctx.pop()
+        pass
 
