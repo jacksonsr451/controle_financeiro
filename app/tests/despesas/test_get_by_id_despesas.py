36da85c1231ef73d36754a5ee057bb1e21e37be4
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-04 15:00:56 -0300

    Test get by id return message error in despesas

--- app/tests/despesas/test_get_by_id_despesas.py
+++ app/tests/despesas/test_get_by_id_despesas.py
@@ -32,6 +32,13 @@ class TestGetByIDDespesa(TestCase):
         response = self.app.get(self.URL + id)
         self.assertEqual(value.get_json(), response.get_json())
     
+    
+    def test_should_be_return_message_error(self):
+        id = "1"
+        value = jsonify({"message": "Registro não existe para este id: {}".format(id)})
+        response = self.app.get(self.URL + id)
+        self.assertEqual(value.get_json(), response.get_json())
+        
         
     def tearDown(self) -> None:
         db.session.remove()
