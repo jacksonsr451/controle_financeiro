63f7682604aee1ab8cacb040beee46c3afd9fdd8
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-04 14:57:13 -0300

    Test get by id receitas

---
+++ app/tests/receitas/test_get_by_id_receitas.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from unittest import TestCase
+
+from flask import jsonify
+
+from app import app
+from app.ext.flask_sqlalchemy import db
+from app.models.receitas_model import ReceitasModel
+
+
+
+class TestGetByIDReceitas(TestCase):
+    URL = "http://localhost:5000/api/v1/receitas/"
+    
+    
+    def setUp(self) -> None:
+        app_test = app.create_app(FORCE_ENV_FOR_DYNACONF="testing")
+        app_test.testing = True
+        self.ctx = app_test.app_context()
+        self.ctx.push()
+        self.app = app_test.test_client()
+        db.create_all()
+    
+    
+    def test_should_be_return_data_by_id(self):
+        data_1 = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        data = ReceitasModel("Primeira receita", "200,00", data_1)
+        db.session.add(data)
+        db.session.commit()
+        id = "1"
+        value = jsonify({"id": 1, "descricao": "Primeira receita", "valor": "200,00", "data": data_1.replace(" ", "T")})
+        response = self.app.get(self.URL + id)
+        self.assertEqual(value.get_json(), response.get_json())
+    
+        
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+        self.ctx.pop()
+        
