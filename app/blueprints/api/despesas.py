37e51883514e837d20a8b619456bf35f0348a750
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-08 13:17:55 -0300

    Test categoria in despesas included in put

--- app/blueprints/api/despesas.py
+++ app/blueprints/api/despesas.py
@@ -13,7 +13,7 @@ despesa_post_request.add_argument("valor", type=str, help="Valor é um campor ob
 despesa_post_request.add_argument("data", help="Data é um campor obrigatório.", required=True)
 
 despesa_put_request = reqparse.RequestParser()
-despesa_post_request.add_argument("categoria", type=str, required=False)
+despesa_put_request.add_argument("categoria", type=str, required=False)
 despesa_put_request.add_argument("descricao", type=str, help="Descricao é um campor obrigatório e do tipo str.", required=True)
 despesa_put_request.add_argument("valor", type=str, help="Valor é um campor obrigatório e do tipo str.", required=True)
 despesa_put_request.add_argument("data", help="Data é um campor obrigatório.", required=True)
