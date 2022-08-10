ee56b8fd8a18cb79b0dd1dff6d317f6b4b674c7a
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-09 23:10:43 -0300

    Test: resume return values

--- app/tests/resumo/test_resumo_return_values.py
+++ app/tests/resumo/test_resumo_return_values.py
@@ -23,6 +23,43 @@ class TestDeleteReceitas(TestCase):
         db.create_all()
         
         
+    def test_should_be_return_values_by_ano_and_mes(self):
+        self.insert_data()
+        value = self.get_value()
+        response = self.app.get(self.URL + "2022/08")
+        self.assertEqual(value.get_json(), response.get_json())
+        
+    
+    @staticmethod
+    def get_value() -> jsonify:
+        return jsonify({
+            "total receitas": "1200,00",
+            "total despesas": "1200,00",
+            "saldo final": "0,00",
+            "total gasto por categoria": {
+                "Alimentação": "400,00",
+                "Saúde": "400,00",
+                "Educação": "400,00"
+            }
+        })
+    
+    
+    @staticmethod
+    def insert_data() -> None:
+        ReceitasModel.add(request={"descricao":"Primeira receita", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        DespesasModel.add(request={"categoria":"Alimentação","descricao":"Primeira despesa", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        ReceitasModel.add(request={"descricao":"Segunda receita", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        DespesasModel.add(request={"categoria":"Saúde","descricao":"Segunta despesa", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        ReceitasModel.add(request={"descricao":"Terceira receita", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        DespesasModel.add(request={"categoria":"Educação","descricao":"Terceira despesa", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        ReceitasModel.add(request={"descricao":"Quarta receita", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        DespesasModel.add(request={"categoria":"Alimentação","descricao":"Quarta despesa", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        ReceitasModel.add(request={"descricao":"Quinta receita", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        DespesasModel.add(request={"categoria":"Saúde","descricao":"Quinta despesa", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        ReceitasModel.add(request={"descricao":"Sexta receita", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        DespesasModel.add(request={"categoria":"Educação","descricao":"Sexta despesa", "valor":"200,00", "data":datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        
+        
     def tearDown(self) -> None:
         db.session.remove()
         db.drop_all()
