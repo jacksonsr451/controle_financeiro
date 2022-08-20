20e3918e77ac3b2c0b301c89cfc2252a9743302e
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-19 22:23:54 -0300

    Update README

--- app/ext/commands.py
+++ app/ext/commands.py
@@ -4,7 +4,7 @@ from app.core.create_superuser import CreateSuperuser
 def init_app(app):
     @app.cli.command("create-superuser")
     def create_superuser():
-        print("Create superuser command")
+        print("Create superuser command:")
         username = input("Username: ")
         email = input("E-Mail: ")
         password = input("Password: ")
