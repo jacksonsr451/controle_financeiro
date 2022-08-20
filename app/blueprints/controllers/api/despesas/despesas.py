af4b00b8962a4d01aeaa2e903481880c5a9d472c
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-08-20 16:09:06 -0300

    Implementing PEP8

--- app/blueprints/controllers/api/despesas/despesas.py
+++ app/blueprints/controllers/api/despesas/despesas.py
@@ -10,30 +10,31 @@ from ....serializer.despesas_schema import DespesasSchema
 from app.models.despesas_model import DespesasModel
 
 
-
 class Despesas(Resource):
     @jwt_required()
     def get(self) -> jsonify:
-        if "descricao" in request.args:
+        if 'descricao' in request.args:
             return self.get_response_on_despesas(
-                DespesasModel.filter_by_descicao(request.args["descricao"]))       
+                DespesasModel.filter_by_descicao(request.args['descricao'])
+            )
         return self.get_response_on_despesas(DespesasModel.all())
-    
-    
+
     @jwt_required()
     def get_response_on_despesas(self, despesas) -> jsonify:
         if len(despesas) > 1:
             return jsonify(DespesasSchema(data=despesas, many=True).data)
         elif len(despesas) == 1:
             return jsonify(DespesasSchema(data=despesas[0]).data)
-        return jsonify({"message": "Não há registros em despesas"})
-    
-    
+        return jsonify({'message': 'Não há registros em despesas'})
+
     @jwt_required()
     def post(self) -> jsonify:
         req_request = DespesasRequest.get()
-        user = UsersModel.get_user_by_email(email=get_jwt_identity()["email"])
+        user = UsersModel.get_user_by_email(email=get_jwt_identity()['email'])
         if DespesasModel.add_user_id(user_id=user.id).add(request=req_request):
-            return jsonify({"message": "Dados inseridos com sucesso"})    
-        return jsonify({"message": "Não é permitido salvar, verifique os dados inseridos e se não são repeditos!"})
-    
+            return jsonify({'message': 'Dados inseridos com sucesso'})
+        return jsonify(
+            {
+                'message': 'Não é permitido salvar, verifique os dados inseridos e se não são repeditos!'
+            }
+        )
--- app/blueprints/controllers/api/users/users_by_id.py
+++ app/blueprints/controllers/api/users/users_by_id.py
@@ -2,7 +2,9 @@ from flask import jsonify
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 
-from app.blueprints.middlewares.superuser_middleware import superuser_middleware
+from app.blueprints.middlewares.superuser_middleware import (
+    superuser_middleware,
+)
 
 from ....serializer.users_schema import UsersSchema
 
@@ -11,29 +13,31 @@ from ....requets.users_request import UsersRequest
 from .....models.users_model import UsersModel
 
 
-
-
 class UsersById(Resource):
     @jwt_required()
     @superuser_middleware
     def get(self, id) -> jsonify:
         despesa = UsersModel.get(id)
-        if despesa is not None: return jsonify(UsersSchema(data=despesa).data)
-        return jsonify({"error": "Registro não existe para este id: {}".format(id)})   
-        
-    
+        if despesa is not None:
+            return jsonify(UsersSchema(data=despesa).data)
+        return jsonify(
+            {'error': 'Registro não existe para este id: {}'.format(id)}
+        )
+
     @jwt_required()
     @superuser_middleware
     def put(self, id) -> jsonify:
         if UsersModel.put(id, UsersRequest.get()):
-            return jsonify({"message": "Usuário atualizado!"})  
-        return jsonify({"error": "Não há registro para usuários de id: {}!".format(id)})
+            return jsonify({'message': 'Usuário atualizado!'})
+        return jsonify(
+            {'error': 'Não há registro para usuários de id: {}!'.format(id)}
+        )
 
-    
-    @jwt_required() 
+    @jwt_required()
     @superuser_middleware
     def delete(self, id) -> jsonify:
         if UsersModel.delete(id=id):
-            return jsonify({"message": "Usuário deletado com sucesso!"})
-        return jsonify({"error": "Não usuário cadastrado com o id: {}!".format(id)})
-            
+            return jsonify({'message': 'Usuário deletado com sucesso!'})
+        return jsonify(
+            {'error': 'Não usuário cadastrado com o id: {}!'.format(id)}
+        )
